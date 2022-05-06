22c6ea149e260f061433288f097c74da21fcdac4
YukiMiyakawa <sakomiya3838@gmail.com>
2022-05-06 16:59:40 +0900

    image set

--- SampleSwing.java
+++ SampleSwing.java
@@ -2,9 +2,18 @@ import javax.swing.*;
 
 public class SampleSwing {
   private static JFrame frame;
+  private static ImageIcon image;
+  private static JLabel label;
 
   public static void main(String[] args) {
     frame = new JFrame();
+    image = new ImageIcon("sample.jpg");
+    label = new JLabel(image);
+
+    frame.getContentPane().add(label);
+    frame.getContentPane().setLayout(null);
+    label.setBounds(0, 0, 640, 480);
+
     frame.setTitle("SampleSwing");
     frame.setSize(640, 480);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
